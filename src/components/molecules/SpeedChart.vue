b4bda0358756b268ed5b4dc575300c0acf6339d4
Herdantio <herdantio.sindhunata@binus.ac.id>
2021-09-23 22:04:42 +0700

    add line chart note: only use v2 chart js

---
+++ src/components/molecules/SpeedChart.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <line-chart :chart-data="dataCollection"></line-chart>
+  </div>
+</template>
+
+<script>
+import LineChart from '../atoms/LineChart'
+import {mapState} from 'vuex'
+
+export default {
+  name: "SpeedChart",
+  components:{
+    LineChart
+  },
+  computed: {
+    ...mapState('CarModule', {
+      dataCollection: state => state.chart
+    })
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
--- src/components/organisms/CarControl.vue
+++ src/components/organisms/CarControl.vue
@@ -1,12 +1,14 @@
 <template>
-  <el-container>
+  <div>
     <el-row>
       <el-button type="success" v-if="isStopped || isPaused" @click="startAction">Start</el-button>
       <el-button type="primary" v-if="isRunning" @click="pauseAction">Pause</el-button>
       <el-button type="danger" @click="stopAction">Stop</el-button>
     </el-row>
-    <el-row></el-row>
-  </el-container>
+    <el-row>
+      <speed-chart></speed-chart>
+    </el-row>
+  </div>
 </template>
 
 <script>
@@ -14,6 +16,9 @@ import {mapState, mapActions} from 'vuex'
 
 export default {
   name: "CarControl",
+  components: {
+    SpeedChart: ()=>import('../molecules/SpeedChart')
+  },
   computed: {
     ...mapState('CarModule', {
       isRunning: state => (state.runStatus == 'isRunning'),
